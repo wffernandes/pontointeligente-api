f32d15713e220dfd3d05056465afd0c4fb5fc1f6
wffernandes <william_fabricio@globo.com>
2021-07-23 19:42:48 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/william/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
