c0fd6c739ba707b8b18baf5dc0abcfca7321a9d8
wffernandes <william_fabricio@globo.com>
2021-07-27 12:19:58 -0300

    Adicionado serviço de empresa, funcionario e lancamento

---
+++ src/main/java/com/william/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.william.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
---
+++ src/main/java/com/william/pontointeligente/api/repositories/FuncionarioRepository.java
@@ -0,0 +1,19 @@
+package com.william.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.william.pontointeligente.api.entities.Funcionario;
+
+@Transactional(readOnly = true)
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+
+	Funcionario findByCpf(String cpf);
+	
+	Funcionario findByEmail(String email);
+	
+	Funcionario findByCpfOrEmail(String cpf, String email);
+
+	Funcionario findOne(Long id);
+	
+}
